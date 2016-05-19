53a01cee6131625a5f87dd1d28962b88514d80f7
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-05-19 13:42:30 +0300

    Register method. Unsubscribe method

--- publish-subscribe/smart-event-bus.js
+++ publish-subscribe/smart-event-bus.js
@@ -1,65 +1,90 @@
 "use strict";
 
-function SmartEventBus() {
-    this.handlers = {};
+class SmartEventBus {
+    constructor() {
+        this.handlers = {};
+    }
+
+    /** Registers callback for processing message. */
+    subscribe(message, callback) {
+        if (!message) {
+            throw "Message is null or empty or not defined.";
+        }
+
+        if (!callback) {
+            throw "Callback is null or empty or not defined.";
+        }
+
+        this.handlers[message] = this.handlers[message] || [];
+        this.handlers[message].push(callback);
+    }
+
+    /** Removes callback from processing message. */
+    unsubscribe(message, callback) {
+        if (!message) {
+            throw "Message is null or empty or not defined.";
+        }
+
+        if (!callback) {
+            throw "Callback is null or empty or not defined.";
+        }
+
+        const handlerList = this.handlers[message];
+
+        if (handlerList && handlerList.length) {
+            const index = handlerList.indexOf(callback);
+            if (index !== -1) {
+                handlerList.splice(index, 1);
+            }
+        }
+    }
+
+    publish(message, data) {
+        if (!message) {
+            throw "Message is null or empty or not defined.";
+        }
+
+        // Data and rest arguments could be empty
+
+        var callbacks = this.handlers[message];
+
+        if (callbacks && callbacks.length) {
+            var args = Array.from(arguments).slice(1);
+
+            callbacks.forEach(function (c) {
+                c.apply(null, args);
+            });
+        }
+    }
+
+    /**
+     * Register subscribers and publishers for object methods.
+     * Each method started with on<MessageName> will be registered as subscriber for MessageName.
+     * Each method started with publish<MessageName> will be replaced with method which publishes MessageName on call.
+     */
+    register(object) {
+        if (object === null || object === undefined) {
+            throw "Object is null or undefined.";
+        }
+
+        Object.keys(object)
+            .filter(prop => prop.startsWith("on") || prop.startsWith("publish"))
+            .forEach(prop => {
+                if (prop.startsWith("on")) {
+                    const messageName = prop.substring(2);
+                    this.subscribe(messageName, data => {
+                        object[prop].call(object, data);
+                    });
+                }
+
+                if (prop.startsWith("publish")) {
+                    const messageName = prop.substring(7);
+                    object[prop] = (data) => {
+                        this.publish(messageName, data);
+                    };
+                }
+            });
+    }
 }
-
-SmartEventBus.prototype.subscribe = function (message, callback) {
-    if (!message) {
-        throw "Message is null or empty or not defined.";
-    }
-
-    if (!callback) {
-        throw "Callback is null or empty or not defined.";
-    }
-
-    this.handlers[message] = this.handlers[message] || [];
-    this.handlers[message].push(callback);
-};
-
-SmartEventBus.prototype.publish = function (message, data) {
-    if (!message) {
-        throw "Message is null or empty or not defined.";
-    }
-
-    // Data and rest arguments could be empty
-
-    var callbacks = this.handlers[message];
-
-    if (callbacks && callbacks.length) {
-        var args = Array.from(arguments).slice(1);
-
-        callbacks.forEach(function (c) {
-            c.apply(null, args);
-        });
-    }
-};
-
-// Register subscribers and publishers for object methods.
-// Each method started with on<MessageName> will be registered as subscriber for MessageName.
-// Each method started with publish<MessageName> will be replaced with method which publishes MessageName on call.
-SmartEventBus.prototype.fulfill = function (object) {
-    if (object === null || object === undefined) {
-        throw "Object is null or undefined.";
-    }
-
-    Object.keys(object)
-        .filter(prop => prop.startsWith("on") || prop.startsWith("publish"))
-        .forEach(prop => {
-            if (prop.startsWith("on")) {
-                const messageName = prop.substring(2);
-                this.subscribe(messageName, data => {
-                    object[prop].call(object, data);
-                });
-            }
-
-            if (prop.startsWith("publish")) {
-                const messageName = prop.substring(7);
-                object[prop] = (data) => {
-                    this.publish(messageName, data);
-                };
-            }
-        });
-};
 
 module.exports = SmartEventBus;
--- publish-subscribe/test/tests.js
+++ publish-subscribe/test/tests.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var expect = require("chai").expect;
 
 var SmartEventBus = require("../smart-event-bus");
@@ -26,5 +28,30 @@ describe("SmartEvenBus", () => {
 
             expect(callCount).to.equal(2);
         });
+    });
+    
+    describe("register", () => {
+        
+        it("should add handler for onXXX methods for object literal", () => {
+            var callCount = 0;
+            const actualMessage = {
+                text: "Hello"
+            };
+            
+            let object = {
+                onData(message) {
+                    expect(message).to.equal(actualMessage);
+                    callCount++;
+                }
+            };
+            
+            const eventBus = new SmartEventBus();
+            eventBus.register(object);
+            
+            eventBus.publish("Data", actualMessage);
+            
+            expect(callCount).to.equal(1);
+        });        
+        
     });
 });
