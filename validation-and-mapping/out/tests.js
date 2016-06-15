f196bfef307dfad1f684f0a0b25a489cb0e7e2bc
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-06-15 21:09:26 +0300

    Sample test

---
+++ validation-and-mapping/out/tests.js
@@ -0,0 +1,7 @@
+"use strict";
+describe("Test", function () {
+    it("should run", function () {
+        console.log("Test run successfuly");
+    });
+});
+//# sourceMappingURL=tests.js.map
---
+++ validation-and-mapping/test/tests.ts
@@ -0,0 +1,7 @@
+import * as assert from "assert";
+
+describe("Test", () => {
+    it("should run", () => {
+        console.log("Test run successfuly");
+    });
+});
