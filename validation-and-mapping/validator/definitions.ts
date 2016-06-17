1a5e2cbd876f5c8f61ddd66659a66bb60431e9d0
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-06-18 01:08:05 +0300

    Change definitions and basic rule class.

--- validation-and-mapping/validator/definitions.ts
+++ validation-and-mapping/validator/definitions.ts
@@ -1,33 +1,61 @@
-export interface ValidationErrors {
+/**
+ * Structure contains validation errors with results of validation.
+ * The validation errors contained in dictionary where key is path to validated value
+ * which may includes property access and indexer operations.
+ * 
+ * Also validation errors may contains optonal summary message with empty-string key describes object-wide errors.
+ */
+export interface ValidationErrorHash {
+    /** Object-wide validation messages. */
+    [""]?: string[];
+    /** Validation messages for properties and indexes of object, including ones with more than one nesting level. */
     [path: string]: string[];
 }
 
-export interface ValidationResult<TOut> {
+/** 
+ * Result of validation and conversion. 
+ * @convertedValue is not null only if validation is passed.
+ */
+export interface ValidationResult<T> {
+    /** Gets a value indicates whether validation and conversion was successful. */
     valid: boolean;
-    value: TOut;
-    errors?: ValidationErrors;
+    /** Gets the converted value. If validation failed the converted value is null. */
+    convertedValue: T;
+    /** Errors messages for object and it's content. If validation was successful the errors is null. */
+    errors?: ValidationErrorHash;
 }
 
-export type ReportErrorFunction = (errorMessage: string) => void;
-
-export interface ValidateAndTransformFunc<TIn, TOut> {
-    (value: TIn, reportError: ReportErrorFunction, entity?: any, rootEntity?: any): any;
-}
-
+/**
+ * Validation context allows reporting errors for current validation stage and
+ * creation nested context of different type.
+ */
 export interface IValidationContext {
+    /** Writes an error message to error accumulator for this context. */
     reportError(errorMessage: string): void;
+    /** Creates nested validation context represents property of the object. */
     property(propertyName: string, errorCallback?: (errorMessage: string) => boolean): IValidationContext;
+    /** Creates nested validation context represents array element access by index. */
     index(index: number, errorCallback?: (errorMessage: string) => boolean): IValidationContext;
 }
 
-export interface IValidationRule<TIn, TOut> {
+/**
+ * Represents a single piece of validation logic.
+ */
+export interface ValidationRule<T> {
+
     /**
-     * Run validation of the specified value with callback to given context.
+     * Run validation logic on specified input value.
      * 
-     * param @value The immediate value being validated.
-     * param @validationContext The validation context for reporting errors.
-     * param @entity The entity which properties are currently validated, may be the nested into root entity.
-     * param @root The root entity passed to validator.run method.
+     * param @context Validation context allows report errors for current value and create context for nested validation.
+     * param @done Callback called when validation and conversion is completed. 
+     *             Accepts convertedValue or null if conversion failed and success parameter indicates whether conversion was successful. 
+     * param @inputValue Input value to validate and convert.
+     * param @validatingObject Object which property or element being validated currently.
+     * param @rootObject Object on which validation was run.
      */
-    run(value: TIn, validationContext: IValidationContext, entity: any, root: any): TOut;
+    run(context: IValidationContext,
+        done: (convertedValue: T, success: boolean) => void,
+        inputValue: any,
+        validatingObject?: any,
+        rootObject?: any): void;
 }
--- validation-and-mapping/validator/rules/rules-base.ts
+++ validation-and-mapping/validator/rules/rules-base.ts
@@ -1,66 +1,152 @@
-import { IValidationRule, ValidateAndTransformFunc, ReportErrorFunction } from "../definitions";
-import ValidationContext from "../validation-context";
+import { ValidationRule, IValidationContext } from "../definitions";
 
 
-export abstract class ChainableRuleRunner<TOut> implements IValidationRule<any, TOut> {
-    rules: ValidateAndTransformFunc<any, TOut>[] = [];
+export abstract class ChainableRuleRunner<T> implements ValidationRule<T> {
+    rules: ValidationRule<T>[] = [];
 
-    run(value: any, validationContext: ValidationContext, entity: any, root: any): TOut {
-        return this.rules
-            .reduce((currentValue, rule) => rule(currentValue, err => validationContext.reportError(err), entity, root), value);
+    protected abstract clone(): this;
+
+    /** Runs all chained rules. */
+    run(context: IValidationContext,
+        done: (convertedValue: T, success: boolean) => void,
+        inputValue: any,
+        validatingObject?: any,
+        rootObject?: any): void {
+        if (!context) {
+            throw new Error("context is required.");
+        }
+        if (!done) {
+            throw new Error("done callback is required.");
+        }
+
+        let allRulesValid = true;
+        let value = inputValue;
+        const rulesToRun = [...this.rules];
+
+        const runRule = () => {
+            const rule = rulesToRun.shift();
+            if (rule) {
+                rule.run(context,
+                    (convertedValue, success) => {
+                        if (success) {
+                            value = convertedValue;
+                        }
+
+                        allRulesValid = allRulesValid && success;
+
+                        // Runs next rule recursively
+                        runRule();
+                    },
+                    value,
+                    validatingObject,
+                    rootObject);
+            }
+            else {
+                done(value, allRulesValid);
+            }
+        };
+
+        runRule();
     }
 
-    withRule(rule: ValidateAndTransformFunc<any, TOut>): this {
-        this.rules.push(rule);
-        return this;
+    protected withRule(rule: ValidationRule<T>): this {
+        if (!rule) {
+            throw new Error("rule is required");
+        }
+
+        const copy = this.clone();
+
+        copy.rules = [...this.rules, rule];
+        return copy;
     }
 
-    required(errorMessage: string = "Value is required"): this {
-        return this.withRule(ChainableRuleRunner.requiredRule(errorMessage));
+    /** Adds a rule which uses custom function for validation and converting. */
+    checkAndConvert(fn: (done: (convertedValue: T, errorMessage?: string) => void, inputValue: any, validatingObject?: any, rootObject?: any) => void) {
+        if (!fn) {
+            throw new Error("Check and convert function is required.");
+        }
+
+        return this.withRule({
+            run(context: IValidationContext,
+                done: (convertedValue: T, success: boolean) => void,
+                inputValue: any,
+                validatingObject?: any,
+                rootObject?: any): void {
+
+                fn((convertedValue, errorMessage) => {
+                    if (errorMessage) {
+                        context.reportError(errorMessage);
+                        done(null, false);
+                    }
+                    else {
+                        done(convertedValue, true);
+                    }
+                },
+                    inputValue,
+                    validatingObject,
+                    rootObject);
+            }
+        });
     }
 
-    transform<TOut>(selector: (value: any, entity?: any, rootEntity?: any) => TOut, errorMessage: string = "Conversion failed"): this {
-        return this.withRule(ChainableRuleRunner.transformRule(selector, errorMessage));
+    /** Fails if input value is null or undefined. */
+    required(errorMessage: string = "Value is required."): this {
+        if (!errorMessage) {
+            throw new Error("Error message is required.");
+        }
+
+        return this.checkAndConvert((done, input) => {
+            if (input === null || input === undefined) {
+                done(null, errorMessage);
+            }
+            else {
+                done(input);
+            }
+        })
+    }
+
+    /** 
+     * Converts input value by applying transformation function.
+     * If transformation function returns null or undefined or throws an error fails with specified error message.
+     */
+    transform(transformFn: (value: any, entity?: any, rootEntity?: any) => T, errorMessage: string = "Conversion failed"): this {
+        if (!transformFn) {
+            throw new Error("Transformation function is required.")
+        }
+        if (!errorMessage) {
+            throw new Error("Error message is required.");
+        }
+
+        return this.checkAndConvert((done, input, obj, root) => {
+            try {
+                const converted = transformFn(input, obj, root);
+                if (converted === null || converted === undefined) {
+                    done(null, errorMessage);
+                }
+                else {
+                    done(converted);
+                }
+            }
+            catch (e) {
+                done(null, errorMessage);
+            };
+        });
     }
 
     must(predicate: (value: any, entity?: any, rootEntity?: any) => boolean, errorMessage: string = "Value is invalid"): this {
-        return this.withRule(ChainableRuleRunner.mustRule(predicate, errorMessage));
-    }
-
-    static mustRule<TIn, TOut>(predicate: (value: TIn, entity?: any, rootEntity?: any) => boolean, errorMessage: string): ValidateAndTransformFunc<TIn, TOut> {
-        return (value: TIn, reportError: ReportErrorFunction, entity: any, rootEntity: any) => {
-            if (!predicate(value, entity, rootEntity)) {
-                reportError(errorMessage);
+        if (!predicate) {
+            throw new Error("Predicate is required.");
+        }
+        if (!errorMessage) {
+            throw new Error("Error message is required.");
+        }
+        return this.checkAndConvert((done, input, obj, root) => {
+            if (!predicate(input, obj, root)) {
+                done(null, errorMessage);
             }
-
-            return value;
-        };
-    }
-
-    static transformRule<TIn, TOut>(selector: (value: TIn, entity?: any, rootEntity?: any) => TOut, errorMessage: string): ValidateAndTransformFunc<TIn, TOut> {
-        return (value: TIn, reportError: ReportErrorFunction, entity: any, rootEntity: any) => {
-            try {
-                const result = selector(value, entity, rootEntity);
-
-                if (result === null || result === undefined) {
-                    reportError(errorMessage);
-                }
-
-                return result;
+            else {
+                done(input);
             }
-            catch (e) {
-                reportError(errorMessage);
-            };
-        };
-    }
-
-    static requiredRule<TIn, TOut>(errorMessage: string): ValidateAndTransformFunc<TIn, TOut> {
-        return (value: TIn, reportError: ReportErrorFunction) => {
-            if (value === null || value === undefined) {
-                reportError(errorMessage);
-            }
-
-            return value;
-        };
+        })
     }
 }
