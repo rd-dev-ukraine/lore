3a32e846d72bf34414bc5c191113374765514269
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2016-07-08 15:08:00 +0300

    Angular2 boilerplate

---
+++ ui-framework-popup/angular-2/app/root.ts
@@ -0,0 +1,12 @@
+import { Component  } from "angular2/core";
+
+@Component({
+    selector: "root",
+    template: `
+    <h1>
+        Hello, Angular 2
+    </h1>
+    `
+})
+export class Root {
+}
---
+++ ui-framework-popup/angular-2/typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="../node_modules/angular2/core.d.ts" />
+/// <reference path="../node_modules/angular2/common.d.ts" />
